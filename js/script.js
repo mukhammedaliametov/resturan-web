22ad62089f93b8cc3ff36fc8fa7ab7f95b1ef4dc
Mukhammedali Ametov <ametovmukhammedali07@gmail.com>
2023-04-29 22:37:00 +0500

    first commit :)

--- js/script.js
+++ js/script.js
@@ -2,7 +2,15 @@
 
 const hamburgerItem = document.querySelector('.fa-bars');
 const navMenu = document.querySelector('.nav');
+const hamburgerEl = document.querySelector('.hamburger');
+const closeNavMenu = document.querySelector('.fa-times');
 
 hamburgerItem.addEventListener('click', () => {
-    navMenu.classList.toggle('active')
+    navMenu.classList.toggle('active');
+    hamburgerEl.classList.toggle('active');
 });
+
+closeNavMenu.addEventListener('click', () => {
+    navMenu.classList.remove('active');
+    hamburgerEl.classList.remove('active');
+})
